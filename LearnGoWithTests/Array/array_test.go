cfb741223176966cc1345f546243141fa2aa44c6
Danielson <danielson7632@gmail.com>
2024-12-23 11:29:42 +0700

    array slice

--- LearnGoWithTests/Array/array_test.go
+++ LearnGoWithTests/Array/array_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -21,6 +24,38 @@ func TestSum(t *testing.T) {
 		if got != want {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
+	})
+
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{3, 4})
+	want := []int{3, 7}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSumAllTail(t *testing.T) {
+
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3, 5}, []int{7, 8, 9, 7})
+		want := []int{8, 16}
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{7, 8, 9, 7})
+		want := []int{0, 16}
+		checkSums(t, got, want)
 	})
 
 }

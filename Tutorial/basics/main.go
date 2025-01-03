5912375ebccd0db825af155e5a3750b56afaf1b8
Danielson <danielson7632@gmail.com>
2025-01-03 10:45:07 +0700

    basics and function

---
+++ Tutorial/basics/main.go
@@ -0,0 +1,43 @@
+package main
+
+func main() {
+	//int
+	// var intNum int = 32767
+	// intNum++
+	// fmt.Println(intNum)
+
+	//float
+	// var floatNum float64 = 12345678.9
+	// fmt.Println(floatNum)
+
+	//float type operation
+	// var floatNum32 float32 = 10.1
+	// var intNum32 float32 = 2
+	// var result float32 = floatNum32 + float32(intNum32)
+	// fmt.Println(result)
+
+	// var intNum1 int = 3
+	// var intNum2 int = 2
+	// fmt.Println(intNum1 / intNum2)
+	// fmt.Println(intNum1 % intNum2)
+
+	//string
+	// var myString = `hello wooddy` + " " + `test 123`
+	// fmt.Println(myString)
+	// fmt.Println(utf8.RuneCountInString(myString))
+
+	//boolean
+	// var myBoolean bool = false
+	// fmt.Println(myBoolean)
+
+	//shorthand
+	// myVar := "test"
+
+	// var1, var2 := 1, 2
+	// fmt.Println(myVar, var1, var2)
+
+	//const
+	// const myConst string = "const value"
+	// fmt.Println(myConst)
+
+}
---
+++ Tutorial/function/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	printValue := "Hello"
+	printMe(printValue)
+
+	numerator := 4
+	denominator := 2
+	var result, remainder, err = intDivision(numerator, denominator)
+
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// } else if remainder == 0 {
+	// 	fmt.Printf("Division result is %v", result)
+	// } else {
+	// 	fmt.Printf("Division result is %v with remainder of %v", result, remainder)
+	// }
+
+	switch {
+	case err != nil:
+		fmt.Println(err.Error())
+	case remainder == 0:
+		fmt.Printf("Division result is %v", result)
+	default:
+		fmt.Printf("Division result is %v with remainder of %v", result, remainder)
+	}
+
+}
+
+func printMe(printValue string) {
+	fmt.Println(printValue)
+}
+
+func intDivision(numerator int, denominator int) (int, int, error) {
+	var err error
+	if denominator == 0 {
+		err = errors.New("cannot divide by zeroo")
+		return 0, 0, err
+	}
+
+	return numerator / denominator, numerator % denominator, err
+}
